48923fc4db5160638c9f5223f9f66464be1538fe
Lucca Maciel <lucca@jajb.net>
2026-09-24 19:27:07 +0200

    ejercicio 1, probando dart

---
+++ relacion1/1hola-mundo.dart
@@ -0,0 +1,14 @@
+/*Ejercicio 1 de la relación 1
+    archivo: 1hola-mudo.dart
+    fecha: 24/09/2026
+    Autor :Lucca Maciel Rodríguez Vázquez*/
+
+    /*void main() {
+      print("Hola mundo");
+    }*/
+    void main() {
+      String nombre = "Lucca"; // tipo y nombre de la variable
+      int veces = 1; // tipo y nombre de la variable
+      print("Hola mundo, mi nombre es $nombre");
+      print("El programa se ha ejecutado ${veces*2-1} vez(es)");// si es una expresion se pone entre llaves se pone entre llaves
+    }
